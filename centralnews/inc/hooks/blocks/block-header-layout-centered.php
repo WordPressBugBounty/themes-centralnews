08a54344c47964763f5f04a2ee6278d8a2485e2d
Sean Murphy <sean@iamseanmurphy.com>
2025-07-06 05:17:19 +0000

    2.0.55

--- centralnews/inc/hooks/blocks/block-header-layout-centered.php
+++ centralnews/inc/hooks/blocks/block-header-layout-centered.php
@@ -11,13 +11,13 @@ $morenews_class = '';
 $morenews_background = '';
 if (has_header_image()) {
     if ($select_header_image_mode == 'above') {
-        $morenews_class = 'af-header-image';    
-      } else {
-      $morenews_class = 'af-header-image data-bg';
-      $morenews_background = get_header_image();
-      // Set inline style for background-image
-      $inline_style = 'style="background-image: url(' . esc_url($morenews_background) . ');"';
-      }
+        $morenews_class = 'af-header-image';
+    } else {
+        $morenews_class = 'af-header-image data-bg';
+        $morenews_background = get_header_image();
+        // Set inline style for background-image
+        $inline_style = 'style="background-image: url(' . esc_url($morenews_background) . ');"';
+    }
 }
 $morenews_show_top_header_section = morenews_get_option('show_top_header_section');
 ?>
@@ -45,11 +45,11 @@ $morenews_show_top_header_section = morenews_get_option('show_top_header_section
 
     <div class="mid-header">
         <div class="container-wrapper">
-        <?php if (has_header_image() && $select_header_image_mode == 'above'): ?>
-    <div class="header-image-above-site-title">
-        <img src="<?php echo esc_url(get_header_image()); ?>" alt="<?php echo esc_attr(get_bloginfo('title')); ?>" />
-        </div>
-    <?php endif; ?>
+            <?php if (has_header_image() && $select_header_image_mode == 'above') : ?>
+                <div class="header-image-above-site-title">
+                    <img src="<?php echo esc_url(get_header_image()); ?>" alt="<?php echo esc_attr(get_bloginfo('title')); ?>" />
+                </div>
+            <?php endif; ?>
             <div class="mid-bar-flex">
                 <div class="logo">
                     <?php do_action('morenews_load_site_branding'); ?>
@@ -61,10 +61,19 @@ $morenews_show_top_header_section = morenews_get_option('show_top_header_section
     <?php
     $morenews_banner_advertisement = morenews_get_option('banner_advertisement_section');
     if (('' != $morenews_banner_advertisement) || is_active_sidebar('home-advertisement-widgets')) {
-    $morenews_banner_advertisement_scope = morenews_get_option('banner_advertisement_scope');
-    if ($morenews_banner_advertisement_scope == 'front-page-only') :
-        if (is_home() || is_front_page()) :
+        $morenews_banner_advertisement_scope = morenews_get_option('banner_advertisement_scope');
+        if ($morenews_banner_advertisement_scope == 'front-page-only') :
+            if (is_home() || is_front_page()) :
     ?>
+                <div class="below-mid-header">
+                    <div class="container-wrapper">
+                        <div class="header-promotion">
+                            <?php do_action('morenews_action_banner_advertisement'); ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endif; ?>
+        <?php else : ?>
             <div class="below-mid-header">
                 <div class="container-wrapper">
                     <div class="header-promotion">
@@ -72,19 +81,16 @@ $morenews_show_top_header_section = morenews_get_option('show_top_header_section
                     </div>
                 </div>
             </div>
-        <?php endif; ?>
-    <?php else : ?>
-        <div class="below-mid-header">
-            <div class="container-wrapper">
-                <div class="header-promotion">
-                    <?php do_action('morenews_action_banner_advertisement'); ?>
-                </div>
-            </div>
-        </div>
-    <?php endif; 
+    <?php endif;
     } ?>
 
 </div>
+<?php if (is_active_sidebar('express-off-canvas-panel')) : ?>
+    <div id="sidr" class="primary-background">
+        <a class="sidr-class-sidr-button-close" href="#sidr-nav"><i class="fa fa-window-close"></i></a>
+        <?php dynamic_sidebar('express-off-canvas-panel'); ?>
+    </div>
+<?php endif; ?>
 <div id="main-navigation-bar" class="bottom-header">
     <div class="container-wrapper">
         <div class="bottom-nav">
@@ -93,10 +99,7 @@ $morenews_show_top_header_section = morenews_get_option('show_top_header_section
                     <div class="off-cancas-panel">
                         <?php do_action('morenews_load_off_canvas'); ?>
                     </div>
-                    <div id="sidr" class="primary-background">
-                        <a class="sidr-class-sidr-button-close" href="#sidr-nav"><i class="fa fa-window-close"></i></a>
-                        <?php dynamic_sidebar('express-off-canvas-panel'); ?>
-                    </div>
+
                 <?php endif; ?>
                 <?php do_action('morenews_action_main_menu_nav'); ?>
                 <div class="search-watch">
@@ -108,4 +111,4 @@ $morenews_show_top_header_section = morenews_get_option('show_top_header_section
 
         </div>
     </div>
-    </div>
+</div>
